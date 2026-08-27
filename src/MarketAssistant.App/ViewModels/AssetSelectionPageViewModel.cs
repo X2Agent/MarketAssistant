203846c2072f6de20580301c515cb979033205db
MayueCif <mayue8373@live.com>
2026-08-27 23:31:44 +0800

    [全库] dotnet format 统一空白与缩进

--- src/MarketAssistant.App/ViewModels/AssetSelectionPageViewModel.cs
+++ src/MarketAssistant.App/ViewModels/AssetSelectionPageViewModel.cs
@@ -246,7 +246,7 @@ public partial class AssetSelectionPageViewModel : ViewModelBase, IDisposable
         {
             OnPropertyChanged(nameof(CurrentInputContent));
             OnPropertyChanged(nameof(CurrentPlaceholder));
-          OnPropertyChanged(nameof(CurrentButtonText));
+            OnPropertyChanged(nameof(CurrentButtonText));
             OnPropertyChanged(nameof(CurrentButtonText));
             OnPropertyChanged(nameof(IsInputAreaVisible));
             OnPropertyChanged(nameof(IsQuickStrategyAreaVisible));
